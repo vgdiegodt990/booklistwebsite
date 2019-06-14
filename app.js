ae391516375b3063f87fcb04e651003f9747d969
diegrem <vgdiego1060@gmail.com>
2019-06-14 14:27:13 -0400

    working on navbar

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 //ROUTES 
-
+app.get("/", function(req, res){
+  res.render("landing");
+})
 
 //SERVER SETUP 
 let port = process.env.PORT;
 if (port == null || port == "") {
+  console.log("Server has started.")
   port = 3000;
 }
 app.listen(port);
